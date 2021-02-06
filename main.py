8e3879fd2b1f59da7fbd781b6b2b0518b144480d
N <pawsey18@gmail.com>
2021-02-06 17:24:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write your code below this row 👇
+
+for i in range(1, 101):
+  if i % 15 == 0: 
+    print("fizzbuzz")
+  elif i % 3 == 0:
+    print("fizz")
+  elif i % 5 == 0: 
+    print("buzz")
+  else:
+    print(i)     
+
+
+
+ 
